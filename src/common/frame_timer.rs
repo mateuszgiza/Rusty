03c588041d13e167327ff93734066f55a344dad2
Mateusz Giza <matgizq@gmail.com>
2018-10-16 15:09:15 +0200

    minor refactor and cleanup in FrameTimer

--- src/common/frame_timer.rs
+++ src/common/frame_timer.rs
@@ -22,24 +22,32 @@ impl FrameTimer {
     pub fn elapsed_time(&self) -> Duration { self.elapsed_time }
 
     pub fn update(&mut self) {
-        self.elapsed_time = self.timer.elapsed() + self.calc_time;
-        let s = Instant::now();
+        let current_frame_time = self.timer.elapsed() + self.calc_time;
+        let calculation_timer = Self::begin_measuring_calculation();
 
-        self.time_to_sleep = Duration::from_nanos(0);
-        if Self::FRAME_TIME > self.elapsed_time {
-            self.time_to_sleep = Self::FRAME_TIME - self.elapsed_time;
-        }
-
-        // self.print();
-
-        ::std::thread::sleep(self.time_to_sleep);
-
-        self.calc_time = s.elapsed() - self.time_to_sleep;
-        self.elapsed_time = self.timer.elapsed();
-        self.timer = Instant::now();
+        self.time_to_sleep = Self::calculate_time_to_sleep(current_frame_time);
+        self.execute_sleep();
+        self.update_times(calculation_timer);        
     }
 
-    fn print(&mut self) {
-        println!("elapsed: {:?} | sleep: {:?} | calc: {:?} | sum: {:?}", self.elapsed_time, self.time_to_sleep, self.calc_time, self.elapsed_time + self.time_to_sleep);
+    fn begin_measuring_calculation() -> Instant {
+        return Instant::now();
+    }
+
+    fn calculate_time_to_sleep(current_frame_time: Duration) -> Duration {
+        if Self::FRAME_TIME > current_frame_time {
+            return Self::FRAME_TIME - current_frame_time;
+        }
+        return Duration::from_nanos(0);
+    }
+
+    fn execute_sleep(&self) {
+        ::std::thread::sleep(self.time_to_sleep);
+    }
+
+    fn update_times(&mut self, calculation_timer: Instant) {
+        self.calc_time = calculation_timer.elapsed() - self.time_to_sleep;
+        self.elapsed_time = self.timer.elapsed();
+        self.timer = Instant::now();
     }
 }
