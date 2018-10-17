a508fa030aa6873274f7b24c3074088ab8164c74
Mateusz Giza <matgizq@gmail.com>
2018-10-17 13:36:40 +0200

    Extract ResultExtension to separate file

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -1,14 +1,2 @@
-pub trait ResultExtensions {
-    fn log_on_error(&self, message: &str);
-}
-
-impl<T, TError> ResultExtensions for Result<T, TError> {
-    fn log_on_error(&self, message: &str) {
-        match self {
-            Err(_error) => {
-                println!("{}", message);
-            },
-            _ => {}
-        }
-    }
-}
+mod result_ext;
+pub use self::result_ext::ResultExtensions;
