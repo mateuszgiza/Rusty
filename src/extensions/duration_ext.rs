bab3299d68e9aac5b65bb7af731df4dee86eaca7
Mateusz Giza <matgizq@gmail.com>
2018-10-17 14:14:36 +0200

    Improve casting extension definition

--- src/extensions/duration_ext.rs
+++ src/extensions/duration_ext.rs
@@ -1,11 +1,11 @@
 use std::time::Duration;
 
 pub trait As<T> {
-    fn as_type<TDest>(&self) -> T where TDest: Into<T>;
+    fn as_type<TDest>(&self) -> TDest where TDest: From<T>;
 }
 
 impl As<f32> for Duration {
-    fn as_type<TDest>(&self) -> f32 where TDest: Into<f32> {
-        self.as_secs() as f32 + self.subsec_nanos() as f32 / 1_000_000_000.0
+    fn as_type<TDest>(&self) -> TDest where TDest: From<f32> {
+        TDest::from(self.as_secs() as f32 + self.subsec_nanos() as f32 / 1_000_000_000.0)
     }
 }
