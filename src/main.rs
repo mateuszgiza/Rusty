5e6cc2d1ae7639689e717faec5a62ad5b4d85f74
Mateusz Giza <matgizq@gmail.com>
2018-11-28 08:30:40 +0100

    Use colored output info

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate chrono;
-extern crate sdl2;
-extern crate specs;
-extern crate sdl2_extras;
+extern crate colored;
 extern crate lazy_static;
+extern crate sdl2;
+extern crate sdl2_extras;
+extern crate specs;
 #[macro_use] extern crate specs_derive;
 
 mod builders;
@@ -13,17 +14,20 @@ mod extensions;
 mod resources;
 mod systems;
 
+use colored::*;
+
 const START_MESSAGE: &'static str = "#: Starting Rusty...";
 const CLOSE_MESSAGE: &'static str = "#: Closing Rusty...";
+const FATAL_ERROR_MSG: &'static str = "[FATAL]";
 
 fn main() {
-    println!("{}", START_MESSAGE);
+    println!("{}", START_MESSAGE.green());
 
     let result = engine::start();
 
     let close_info = match result {
-        Ok(_) => CLOSE_MESSAGE.into(),
-        Err(e) => format!("FATAL ERROR: {}", e)
+        Ok(_) => format!("{}", CLOSE_MESSAGE.green()),
+        Err(e) => format!("{} {}", FATAL_ERROR_MSG.red(), e.to_string().bright_red())
     };
 
     println!("{}", close_info);
