c424a41d54864257557d5f8641adfc16a7dfd8c9
Mateusz Giza <matgizq@gmail.com>
2018-12-07 17:40:50 +0100

    Start on event handlers functionality (WiP)

--- src/managers/event_manager.rs
+++ src/managers/event_manager.rs
@@ -1,18 +1,22 @@
+use std::collections::HashMap;
 use sdl2::{
     Sdl,
     EventPump,
-    event::Event,
+    event::{Event, EventType},
     keyboard::Keycode,
 };
 use std::error::Error;
+use std::borrow::Borrow;
 
 pub enum EventProcessStatus {
     Ok,
-    Exit
+    Exit,
+    UnknownEventType
 }
 
 pub struct EventManager {
-    event_pump: EventPump
+    event_pump: EventPump,
+    registered_handlers: HashMap<EventType, Vec<Box<Fn(&Event)>>>
 }
 
 unsafe impl Send for EventManager {}
@@ -21,26 +25,48 @@ unsafe impl Sync for EventManager {}
 impl EventManager {
     pub fn new(sdl_context: &Sdl) -> Result<Self, Box<Error>> {
         let event_manager = EventManager {
-            event_pump: sdl_context.event_pump()?
+            event_pump: sdl_context.event_pump()?,
+            registered_handlers: HashMap::new()
         };
         Ok(event_manager)
     }
 
+    pub fn register<T>(&mut self, event_type: EventType, handler: Box<Fn(&Event)>) {
+        let handlers = self.get_event_handlers(event_type);
+        handlers.push(handler);
+    }
+
     pub fn process_events(&mut self) -> EventProcessStatus {
-        for event in self.event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => return EventProcessStatus::Exit,
-                Event::MouseMotion { x, y, .. } => {
-                    // cursor_rect.set_x(x);
-                    // cursor_rect.set_y(y);
-                },
-                _ => {}
+        let event_iterator = self.event_pump.poll_iter();
+
+        for event in event_iterator {
+            let event_type = Self::get_event_type(&event);
+            if event_type.is_none() {
+                return EventProcessStatus::UnknownEventType;
+            }
+
+            let event_type = event_type.unwrap();
+            if self.registered_handlers.contains_key(&event_type) {
+                let handlers = self.get_event_handlers(event_type);
+                for handler in handlers {
+                    handler(&event);
+                }
             }
         }
 
         EventProcessStatus::Ok
+    }
+
+    fn get_event_handlers(&mut self, event_type: EventType) -> &mut Vec<Box<Fn(&Event)>> {
+        self.registered_handlers.entry(event_type).or_insert(vec![])
+    }
+
+    fn get_event_type(event: &Event) -> Option<EventType> {
+        match event {
+            Event::Quit {..} => Some(EventType::Quit),
+            Event::KeyDown {..} => Some(EventType::KeyDown),
+            Event::MouseMotion {..} => Some(EventType::MouseMotion),
+            _ => None
+        }
     }
 }
